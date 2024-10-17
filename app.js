fa8a709ee12243919b640b4b5c7b47692a878cb1
Ramees <ramees.kr@outlook.com>
2024-10-17 16:17:33 -0400

    corrected import

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { check, validationResult } = require("express-validator");
+const { join } = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
